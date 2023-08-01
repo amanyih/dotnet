03282d9f608f7e395cbde2cc6c0c10167c04d93c
Amanuel Yihune <105606795+amanyih@users.noreply.github.com>
2023-08-01 17:27:09 +0300

    Create Word_count.cs

---
+++ Word_count.cs
@@ -0,0 +1,20 @@
+string word = Console.Writeline("enter a word");
+
+
+Dictionary<string,int> counter(string sent){
+  Dictionary<string,int> dict = Dictionary<,string,ont>();
+  string[] words = sent.Split(new char[] { ' ' });
+}
+
+foreach ( string word in words ){
+  if(dict.Containskey(word){
+    dict[word]++;
+}
+     else{ dict[word] =1;}
+return dict;
+
+         
+
+      
+    
+     }
