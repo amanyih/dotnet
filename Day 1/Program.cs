936f6500d4f625c0006cfc87c7b68fb93c90d68a
amanuel <amanuelyihunie27@gmail.com>
2023-08-02 15:42:10 +0300

    [Day1] restructure day1 solution

---
+++ Day 1/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+namespace Day1
+{
+    class Program {
+
+        static void Main(string[] args)
+        {
+            Conosle.WriteLine("Enter a word");
+            var word = Console.ReadLine();
+            Conosle.WriteLine(isPalindrome(word));
+            
+    }
+
+    static bool isPalindrome(string word){
+       
+  int i = 0;
+  int j= 0;
+
+  while (i<=j){
+    if(word[i] == word[j]){
+      i ++;
+      j--;
+    }
+    else{
+      return false;
+    }
+  }
+       return true;
+ 
+  
+    }
+
+    
+}}
--- Day 1/Word_count.cs
+++ Day 1/Word_count.cs
@@ -2,12 +2,12 @@ string word = Console.Writeline("enter a word");
 
 
 Dictionary<string,int> counter(string sent){
-  Dictionary<string,int> dict = Dictionary<,string,ont>();
+  Dictionary<string,int> dict = Dictionary<string,int>();
   string[] words = sent.Split(new char[] { ' ' });
 }
 
 foreach ( string word in words ){
-  if(dict.Containskey(word){
+  if(dict.Containskey(word)){
     dict[word]++;
 }
      else{ dict[word] =1;}
